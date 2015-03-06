776169da3d159cb74a01e71a1fa5a2cbc339935e
jgrant95 <jongrant95@gmail.com>
2015-03-06 10:33:54 +0000

    key tracker start
    This is the basic key tracking

--- GroupProject/GroupProject/Form1.cs
+++ GroupProject/GroupProject/Form1.cs
@@ -16,5 +16,21 @@ namespace GroupProject
         {
             InitializeComponent();
         }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //uses char to contain the key entry - strings will allow for storing of words too later
+            char c;
+            c = e.KeyChar;
+            //adds the key entry into a label
+            label1.Text = label1.Text + c.ToString();
+
+            //uses an if statement to determine if the return, space or fullstop key is pressed
+            if (e.KeyChar == Convert.ToChar(Keys.Return) || c == ' ' || c == '.')
+            {
+                //resets label
+                label1.Text = "";
+            }
+        }
     }
 }
