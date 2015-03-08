1e06f90d38fccfbae27d7d5abbc394c126ea323b
jgrant95 <jongrant95@gmail.com>
2015-03-08 17:18:17 +0000

    Updated key track
    Now acknowledges the backspace key - also has if statements in place to
    ensure no errors are incurred. I haven't managed to think of how to
    combat the delete key and how the arrow keys are in place - this may be
    difficult to remove - I'll try put it as an issue on github

--- GroupProject/GroupProject/Form1.cs
+++ GroupProject/GroupProject/Form1.cs
@@ -22,15 +22,32 @@ namespace GroupProject
             //uses char to contain the key entry - strings will allow for storing of words too later
             char c;
             c = e.KeyChar;
-            //adds the key entry into a label
-            label1.Text = label1.Text + c.ToString();
 
-            //uses an if statement to determine if the return, space or fullstop key is pressed
-            if (e.KeyChar == Convert.ToChar(Keys.Return) || c == ' ' || c == '.')
+            //makes back key not key entries - but delete the last char of the string
+            if (e.KeyChar == Convert.ToChar(Keys.Back))
             {
-                //resets label
-                label1.Text = "";
+                if (label1.Text != "") //ensures no errors will be incurred by checking label if empty
+                    label1.Text = label1.Text.Remove(label1.Text.Length - 1);//removes last char from string
             }
+            //else if (e.KeyChar == Convert.ToChar(Keys.Delete))
+            //{
+            //    if (label1.Text != "") //ensures no errors will be incurred by checking label if empty
+            //        label1.Text = label1.Text.Remove(label1.Text.Length - 1);//removes last char from string
+            //}
+            else
+            { 
+                //adds the key entry into a label
+                label1.Text = label1.Text + c.ToString();
+            }
+
+            //uses an if statement to determine if the return or fullstop key is pressed
+            if (e.KeyChar == Convert.ToChar(Keys.Return) || c == '.')
+            {
+                label1.Text = ""; //resets label
+            }
+
+            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
